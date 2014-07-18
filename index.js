82f0a2efbf2e634f0aef105fdd98f804a8cc7439
Scott Corgan <scottcorgan@gmail.com>
2014-07-18 15:30:07 -0700

    Do not add root when none is provided

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ var path = require('path');
 module.exports = function (filepath, options) {
   options = options || {};
   
-  var root  = options.root || './';
-  var fullpath = path.join(root, filepath);
+  var root  = options.root;
+  var fullpath = (root) ? path.join(root, filepath) : filepath;
   
   if (!fs.existsSync(fullpath)) return false;
   if (!fs.statSync(fullpath).isFile()) return false;
