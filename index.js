4842b9577743742e76581e68908368d3113df2d0
Scott Corgan <scottcorgan@gmail.com>
2014-07-22 11:36:16 -0700

    spaces

--- index.js
+++ index.js
@@ -4,8 +4,11 @@ var path = require('path');
 module.exports = function (filepath, options) {
   options = options || {};
   
+  if (!filepath) return false;
+  
   var root  = options.root;
   var fullpath = (root) ? path.join(root, filepath) : filepath;
+  
   
   if (!fs.existsSync(fullpath)) return false;
   if (!fs.statSync(fullpath).isFile()) return false;
