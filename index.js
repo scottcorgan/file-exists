2915cfcbd7f1ee872557be24a54d3ecc1f5e5d78
Scott Corgan <scottcorgan@gmail.com>
2015-11-20 10:36:53 -0800

    release 1.0.0
    - update tap-spec, tape
    - remove deprecated fs code
    - remove semicolons, cuz who needs ‘em?

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-var fs = require('fs');
-var path = require('path');
+var fs = require('fs')
+var path = require('path')
 
 module.exports = function (filepath, options) {
-  options = options || {};
-  
-  if (!filepath) return false;
-  
-  var root = options.root;
-  var fullpath = (root) ? path.join(root, filepath) : filepath;
-  
+  options = options || {}
+
+  if (!filepath) return false
+
+  var root = options.root
+  var fullpath = (root) ? path.join(root, filepath) : filepath
+
   try {
-    return fs.statSync(fullpath).isFile();
+    return fs.statSync(fullpath).isFile()
   } catch (e) {
-    return false;
+    return false
   }
-};
+}
