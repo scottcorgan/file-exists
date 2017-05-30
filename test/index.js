5df24e27bdb2f24fb7921428ce3cf750e9b5dad5
Perry Mitchell <perry@kiosked.com>
2017-05-30 12:01:30 +0300

    Add tests for async non-existing file

--- test/index.js
+++ test/index.js
@@ -27,6 +27,13 @@ test('async', t => {
         t.notOk(exists, 'directory is not a file')
         done()
       })
+    },
+    done => {
+      fileExists('not.here', (err, exists) => {
+        t.notOk(err, 'non-existing file doesn\'t throw')
+        t.notOk(exists, 'non-existing file doesn\'t exist')
+        done()
+      })
     }
   ], err => {
     rmdir('.tmp', () => t.end())
@@ -40,6 +47,7 @@ test('sync', t => {
   t.ok(fileExists.sync('.tmp/index.html'), 'file does exist')
   t.ok(fileExists.sync('/index.html', {root: '.tmp'}), 'file exists in given root directory')
   t.notOk(fileExists.sync('.tmp'), 'directory is not a file')
+  t.notOk(fileExists.sync('not.here'), 'non-existing file doesn\'t exist')
 
   rmdir('.tmp', () => t.end())
 })
